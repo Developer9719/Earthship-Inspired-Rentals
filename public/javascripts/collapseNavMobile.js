4fb7dc8099dc7435695dfcc50f559b063b045457
Justin Battrick <justin.bat19@hotmail.com>
2024-12-11 19:52:25 -0500

    Initialized the JS for the hamburger menu on mobile

---
+++ public/javascripts/collapseNavMobile.js
@@ -0,0 +1,5 @@
+/* Default Mode: Collapsed
+* In collapsed mode there is a symbol (most likely a hamburger menu symbol) shown centered in the middle 
+* In expanded mode the pages are all shown vertically
+*/
+
